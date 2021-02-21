7157e710be88e63a7e7d52728c0a7329b1335c65
shushmatec <shushmapatil.sp29@gmail.com>
2021-02-21 15:27:02 +0530

    initial commit

---
+++ cruddemo/src/main/java/com/springboot/cruddemo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.springboot.cruddemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
---
+++ cruddemo/src/test/java/com/springboot/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,8 @@
+package com.springboot.cruddemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+}
